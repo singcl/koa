ac7f13635811077b683ce292b99599a865be75ab
singcl <24661881@qq.com>
2018-03-29 13:17:16 +0800

    :rotating_light: add eslint file and so on

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+      "max-len": ["error", { "comments": 200 }],
+      "linebreak-style": 0,
+      "indent": ["error", 4],
+      "no-var": "off",
+      "semi": "off",
+      "max-len": ["error", 150],
+      "no-multi-spaces": ["error", { ignoreEOLComments: true }],
+      "no-console": 0
+    }
+};
