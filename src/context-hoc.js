67bc92008771127970c9a2671cac6cafc27ae88d
singcl <24661881@qq.com>
2018-03-30 11:52:36 +0800

    :sparkles: hoc context

---
+++ src/context-hoc.js
@@ -0,0 +1,52 @@
+/**
+ * 最终的一个引用关系：
+ * context 原型对象
+    const ctx = Object.create(this.context);
+    ctx.request = Object.create(this.request);
+    ctx.response = Object.create(this.response);
+    ctx.request.req = req;
+    ctx.response.res = res;
+    ctx.req = req;
+    ctx.res = res;
+ */
+
+// context 原型对象
+const context = {}
+
+/**
+ * 代理设置对象的getter
+ * 
+ * @param {Object} property request/response 对象实例 
+ * @param {String} name     属性名
+ */
+function delegateGet(property, name) {
+    context.__defineGetter__(name, function() {
+        return this[property][name]
+    })
+}
+
+/**
+ * 代理设置对象的setter
+ * 
+ * @param {Object} property request/response 对象实例 
+ * @param {String} name     属性名
+ */
+function delegateSet(property, name) {
+    context.__defineSetter__(name, function(val) {
+        this[property][name] = val
+    })
+}
+
+const requestSet = []
+const requestGet = ['query']
+
+const responseSet = ['body', 'status']
+const responseGet = responseSet
+
+requestSet.forEach(item => delegateSet('request', item))
+requestGet.forEach(item => delegateGet('request', item))
+
+responseSet.forEach(item => delegateSet('response', item))
+responseGet.forEach(item => delegateGet('response', item))
+
+module.exports = context
